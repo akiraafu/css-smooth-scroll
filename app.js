9700d86f262a45ddfeaa38ba84710de8d221feae
Akira <junrongfu6@gmail.com>
2024-09-05 21:46:58 +0800

    handle mobile device

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const track = document.getElementById("image-track");
 
-window.onmousemove = (e) => {
+const handleMove = (clientX) => {
   if (track.dataset.mouseDownAt === "0") return;
 
-  const mouseData = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+  const mouseData = parseFloat(track.dataset.mouseDownAt) - clientX;
   const maxData = window.innerWidth / 2;
   const percentage = (mouseData / maxData) * -100,
     nextPercentageUnconstrained =
@@ -26,41 +26,19 @@ window.onmousemove = (e) => {
       { duration: 1200, fill: "forwards" }
     );
   }
-
-  //   if (track.dataset.mouseDownAt === "0") return;
-
-  //   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-  //     maxDelta = window.innerWidth / 2;
-
-  //   const percentage = (mouseDelta / maxDelta) * -100,
-  //     nextPercentageUnconstrained =
-  //       parseFloat(track.dataset.prevPercentage) + percentage,
-  //     nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-
-  //   track.dataset.percentage = nextPercentage;
-
-  //   track.animate(
-  //     {
-  //       transform: `translate(${nextPercentage}%, -50%)`,
-  //     },
-  //     { duration: 1200, fill: "forwards" }
-  //   );
-
-  //   for (const image of track.getElementsByClassName("image")) {
-  //     image.animate(
-  //       {
-  //         objectPosition: `${100 + nextPercentage}% center`,
-  //       },
-  //       { duration: 1200, fill: "forwards" }
-  //     );
-  //   }
 };
 
-window.onmousedown = (e) => {
-  track.dataset.mouseDownAt = e.clientX;
+window.onmousemove = (e) => handleMove(e.clientX);
+window.ontouchmove = (e) => handleMove(e.touches[0].clientX);
+
+const handleStart = (clientX) => {
+  track.dataset.mouseDownAt = clientX;
 };
 
-window.onmouseup = () => {
+window.onmousedown = (e) => handleStart(e.clientX);
+window.ontouchstart = (e) => handleStart(e.touches[0].clientX);
+
+window.onmouseup = window.ontouchend = () => {
   track.dataset.mouseDownAt = "0";
   track.dataset.prevPercentage = track.dataset.percentage;
 };
